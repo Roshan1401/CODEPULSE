e8b6daec896203d489c4b89edabd0ada0ff4943d
Roshan Patil <patilroshan9687@gmail.com>
2026-04-20 11:27:18 +0530

    give the height to the bar chart and make responsive

--- frontend/src/components/Profile/ProfileState/BarChart.tsx
+++ frontend/src/components/Profile/ProfileState/BarChart.tsx
@@ -7,7 +7,7 @@ interface BarChartProps {
 
 export default function BarChart2({ languages, getColor }: BarChartProps) {
   return (
-    <div className="h-full w-full">
+    <div className="flex h-70 w-60 items-center justify-center md:h-90 md:w-80 lg:h-70 lg:w-50 xl:h-90 xl:w-70">
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={languages}>
           <Tooltip />
